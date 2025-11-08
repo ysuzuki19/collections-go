7fad633bef27c26a43fdb40ff441d465e6007f68
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-11-09 03:17:33 +0900

    move e2e

--- constructor_test.go
+++ constructor_test.go
@@ -1,16 +1,15 @@
-package e2e_test
+package collections_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
 	"github.com/ysuzuki19/collections-go"
+	"github.com/ysuzuki19/collections-go/set"
 )
 
 func TestSet(t *testing.T) {
 	s := collections.NewSet(1, 2, 3)
-	v := s.ToSlice()
-	expected := []int{1, 2, 3}
-	require.Equal(t, expected, v)
+	expected := set.New(1, 2, 3)
+	require.Equal(t, expected, s)
 }
